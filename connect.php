2128ba597f3028cc4e0af9ca0ec8ef8d51dad83d
Margarita Cordova <helencordovar@gmail.com>
2022-05-17 17:35:07 -0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,23 @@
+<?php
+
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+
+    //Database connection
+    $conn = new mysqli('localhost','root','GOOGLE76','test');
+    if($conn->connect_error){
+        echo "$conn->connect_error";
+        die('Connection Failed : '.$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into registration(fullname, email, subject)")
+        $stmt->bind_param("sss",$fullname, $email, $subject);
+        $execval = $stmt->execute();
+		echo $execval;
+        echo "Registration Successful...";
+        $stmt->close();
+        $conn->close();
+    }
+
+?>
+
